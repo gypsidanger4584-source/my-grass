5e2815b1caa0189ee3d7d361e11693fd8eb57ad3
Shensuous <gypsidanger4584@gmail.com>
2026-06-30 13:43:40 +0900

    [level 0] Title: 짝수의 합, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120831. 짝수의 합/짝수의 합.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int i = 1;
+        while(i <= n){
+            if (i % 2 == 0){
+                answer += i;
+            }
+            i++;
+        }
+            return answer;
+    }
+}
