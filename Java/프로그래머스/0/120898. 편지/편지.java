d2b5e65bb481b6fe3737480da96ea15b9e3f16cc
Shensuous <gypsidanger4584@gmail.com>
2026-07-07 19:55:11 +0900

    [level 0] Title: 편지, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120898. 편지/편지.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        char[] ch = message.toCharArray();
+        answer = message.length()*2;
+        return answer;
+    }
+}
