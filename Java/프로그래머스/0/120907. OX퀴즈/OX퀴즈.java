7db084e022f8b5107d78bc9fe48284015c91d149
Shensuous <gypsidanger4584@gmail.com>
2026-07-08 10:04:53 +0900

    [level 0] Title: OX퀴즈, Time: 0.16 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        
+        for(int i = 0; i < quiz.length; i++){
+            String[] part = quiz[i].split(" ");
+            
+            int x = Integer.parseInt(part[0]);
+            String operation = part[1];
+            int y = Integer.parseInt(part[2]);
+            int z = Integer.parseInt(part[4]);
+            
+            int result = operation.equals("+") ? (x+y) : (x-y);
+            answer[i] = (result == z) ? "O" : "X";
+        }
+        return answer;
+    }
+}
