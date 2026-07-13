4878c221db60ddffad88c545a07547c440f51b32
Shensuous <gypsidanger4584@gmail.com>
2026-07-13 13:24:22 +0900

    [level 0] Title: 외계어 사전, Time: 0.03 ms, Memory: 80.1 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String[] spell, String[] dic) {
+        for(String word : dic){
+            if(word.length() != spell.length){
+                continue;
+            }
+            boolean isMatch = true;
+            for(String s : spell){
+                if(!word.contains(s)){
+                    isMatch = false;
+                    break;
+                }
+            }
+            if(isMatch){
+                return 1;
+            }
+        }
+        return 2;
+    }
+}
