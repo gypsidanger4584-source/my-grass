8486980932273cb33d70355182de6379724b33ce
Shensuous <gypsidanger4584@gmail.com>
2026-07-20 14:59:45 +0900

    [level 0] Title: 옹알이 (1), Time: 0.51 ms, Memory: 87.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        for(String word : babbling){
+            word = word.replace("aya"," ");
+	        word = word.replace("ye"," ");
+	        word = word.replace("woo"," ");
+	        word = word.replace("ma"," ");
+            if(word.trim().length() == 0){
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
