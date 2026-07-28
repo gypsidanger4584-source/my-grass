1fc010ca42a5046cf42aaede520f631a8ff9ad45
Shensuous <gypsidanger4584@gmail.com>
2026-07-28 21:19:23 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean solution(int x) {
+        int sum = 0;
+        int ogx = x;
+        while(x > 0){
+            sum += x%10;
+            x/=10;
+        }
+        return ogx%sum==0;
+    }
+}
