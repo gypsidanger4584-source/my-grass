68bcb88444b7681e58f8195f810180108bd1bcf3
Shensuous <gypsidanger4584@gmail.com>
2026-08-11 22:49:43 +0900

    [level 1] Title: [1차] 비밀지도, Time: 4.23 ms, Memory: 82.6 MB -BaekjoonHub

---
+++ Java/프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        for (int i = 0; i < n; i++) {
+            String bin = Integer.toBinaryString(arr1[i] | arr2[i]);
+            bin = String.format("%" + n + "s", bin).replace(' ', '0');
+            bin = bin.replace('1', '#').replace('0', ' ');
+            answer[i] = bin;
+        }
+        return answer;
+    }
+}
