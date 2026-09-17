b36ea6767949a03a84b6bc5a41c70f557c4c56f2
Shensuous <gypsidanger4584@gmail.com>
2026-09-17 18:21:49 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.02 ms, Memory: 82.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String num_str) {
+        int result = 0;
+        for(int i = 0; i < num_str.length(); i++){
+            char c = num_str.charAt(i);
+            result += (c -'0');
+        }
+        return result;
+        
+    }
+}
