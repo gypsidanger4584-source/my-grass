6ce79fff7b2a2fad86c10eaa78198ac0692b5697
Shensuous <gypsidanger4584@gmail.com>
2026-09-18 23:37:17 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 11.75 ms, Memory: 74.1 MB -BaekjoonHub

--- Java/프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
+++ Java/프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
 class Solution {
     public int[] solution(int[] num_list) {
-        Arrays.sort(num_list);
-        return Arrays.copyOfRange(num_list,5,num_list.length);
+        return Arrays.stream(num_list)
+            .sorted()
+            .skip(5)
+            .toArray();
     }
 }
