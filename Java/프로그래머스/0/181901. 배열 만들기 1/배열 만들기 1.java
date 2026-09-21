714337053fe370ea3460d480d20aab79df2afad7
Shensuous <gypsidanger4584@gmail.com>
2026-09-21 20:17:37 +0900

    [level 0] Title: 배열 만들기 1, Time: 5.34 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class Solution {
+    public int[] solution(int n, int k) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= n; i++){
+            if(i % k ==0){
+                list.add(i);
+            }
+        }
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
