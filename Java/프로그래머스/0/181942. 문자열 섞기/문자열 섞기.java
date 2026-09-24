b85f542387acb3d0eedc710602f27875079db9b3
Shensuous <gypsidanger4584@gmail.com>
2026-09-24 19:24:11 +0900

    [level 0] Title: 문자열 섞기, Time: 10.09 ms, Memory: 83.3 MB -BaekjoonHub

--- Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ Java/프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,10 +1,9 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 class Solution {
     public String solution(String str1, String str2) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < str1.length(); i++){
-            sb.append(str1.charAt(i));
-            sb.append(str2.charAt(i));
-        }
-        return sb.toString();
+        return IntStream.range(0,str1.length())
+            .mapToObj(i -> ""+str1.charAt(i)+str2.charAt(i))
+            .collect(Collectors.joining());
     }
 }
