2e07e3a44733acfe0aee533549ef3061167e801f
Shensuous <gypsidanger4584@gmail.com>
2026-09-24 21:39:55 +0900

    [level 0] Title: 문자열 묶기, Time: 12.69 ms, Memory: 118 MB -BaekjoonHub

--- Java/프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ Java/프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -1,12 +1,12 @@
-import java.util.Collections;
 import java.util.HashMap;
+import java.util.Collections;
 class Solution {
     public int solution(String[] strArr) {
         HashMap<Integer,Integer> map = new HashMap<>();
         for(String str : strArr){
-            int len = str.length();
+            int len = str.length();;
             map.put(len,map.getOrDefault(len,0)+1);
-                }
-        return Collections.max(map.values());
-            }
         }
+        return Collections.max(map.values());
+    }
+}
